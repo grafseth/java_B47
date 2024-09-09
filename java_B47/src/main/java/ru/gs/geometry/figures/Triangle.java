ca20d0ed006910f446c946cb49c2922c3632c55e
grafseth <spirit-47@ya.ru>
2024-09-09 20:20:25 +0300

    Задание №3

--- java_B47/src/main/java/ru/gs/geometry/figures/Triangle.java
+++ java_B47/src/main/java/ru/gs/geometry/figures/Triangle.java
@@ -1,38 +1,43 @@
 package ru.gs.geometry.figures;
 
-public class Triangle {
+public final class Triangle {
 
-    private final double side1;
-    private final double side2;
-    private final double side3;
+    private static double side1;
+    private static double side2;
+    private static double side3;
 
     public Triangle(double side1, double side2, double side3) {
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
+        if (side1 < 0 || side2 < 0 || side3 <0) {
+            throw new IllegalArgumentException("Triangle side should be non-negative");
+        }
+if (side1 + side2 < side3 || side2 + side3 < side1 || side3 + side1 < side2) {
+    throw new IllegalArgumentException("Triangle side equality error");
+}
+        Triangle.side1 = side1;
+        Triangle.side2 = side2;
+        Triangle.side3 = side3;
     }
 
     public static void printTriangleArea(Triangle triangle) {
-        String text = String.format("Triangle area withs sides %f, %f and %f = %f", triangle.side1, triangle.side2, triangle.side3, triangle.area());
+        String text = String.format("Triangle area withs sides %f, %f and %f = %f", side1, side2, side3, triangle.area());
         System.out.println(text);
     }
 
     public static void printTrianglePerimeter(Triangle triangle) {
-        String text = String.format("Triangle perimeter withs sides %f, %f and %f = %f", triangle.side1, triangle.side2, triangle.side3, triangle.perimeter());
+        String text = String.format("Triangle perimeter withs sides %f, %f and %f = %f", side1, side2, side3, triangle.perimeter());
         System.out.println(text);
     }
 
     public double area() {
         double s = semiPerimeter();
-        return Math.sqrt(s *(s -side1)*(s -side2)*(s -side3));
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
     }
 
     public double perimeter() {
-        return this.side1 + this.side2 + this.side3;
+        return side1 + side2 + side3;
     }
 
     public double semiPerimeter() {
-        return perimeter()/2;
+        return perimeter() / 2;
     }
-
 }
--- java_B47/src/test/java/ru/gs/geometry/figures/TriangleTest.java
+++ java_B47/src/test/java/ru/gs/geometry/figures/TriangleTest.java
@@ -6,15 +6,33 @@ import org.junit.jupiter.api.Test;
 public class TriangleTest {
 
     @Test
-    void canCalculateArea() {
+    public void canCalculateArea() {
         var t = new Triangle(3.0, 5.0, 7.0);
         double result = t.area();
         Assertions.assertEquals(6.49519052838329, result);
     }
-@Test
+
+    @Test
     void canCalculatePerimeter() {
-        var t = new Triangle(3.0,5.0,5.0);
-        Assertions.assertEquals(13.0, new Triangle (3.0,5.0,5.0).perimeter());
+        var t = new Triangle(3.0, 5.0, 5.0);
+        Assertions.assertEquals(13.0, new Triangle(3.0, 5.0, 5.0).perimeter());
     }
 
+    @Test
+    void cannotCreateTriangleWithNegativeSide() {
+        try {
+            new Triangle(-5.0, -3.0, -3.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithIncorrectSide() {
+        try {
+        new Triangle(3.0, 3.0, 7.0);
+        Assertions.fail();
+    } catch (IllegalArgumentException exception) {
+        }
+    }
 }
