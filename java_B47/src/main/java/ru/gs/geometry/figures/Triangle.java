5e0941bf52e894a36cb9ca356e89b5edb9c7caa3
grafseth <spirit-47@ya.ru>
2024-09-10 20:35:19 +0300

    Задание №3 исправление 1

--- java_B47/src/main/java/ru/gs/geometry/figures/Triangle.java
+++ java_B47/src/main/java/ru/gs/geometry/figures/Triangle.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 
 public record Triangle(double side1, double side2, double side3) {
 
-
-    public Triangle {
-        if (side1 < 0 || side2 < 0 || side3 < 0) {
+    public Triangle (double side1, double side2, double side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+            if (side1 < 0 || side2 < 0 || side3 < 0) {
             throw new IllegalArgumentException("Triangle side should be non-negative");
         }
         if (side1 + side2 < side3 || side2 + side3 < side1 || side3 + side1 < side2) {
