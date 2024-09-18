e3e84d5ad08ee338b900d3baeca76c0fd4a85f9c
grafseth <spirit-47@ya.ru>
2024-09-18 19:44:39 +0300

    Бэкап

--- java_B47/addressbook_web_tests/src/test/java/Manager/ContactsHelper.java
+++ java_B47/addressbook_web_tests/src/test/java/Manager/ContactsHelper.java
@@ -30,6 +30,19 @@ public class ContactsHelper extends HelperBase {
         returnToHomePage();
     }
 
+    private void fillContactForm(ContactData contact) {
+        type(By.name("firstname"), contact.firstname());
+        type(By.name("middlename"), contact.middlename());
+        type(By.name("lastname"), contact.lastname());
+        type(By.name("nickname"), contact.nickname());
+        type(By.name("home"), contact.home());
+    }
+
+    public boolean isContactPresent() {
+        openContactsPage();
+        return manager.isElementPresent(By.name("selected[]"));
+    }
+
     private void removeSelectedContact() {
         click(By.name("Delete"));
     }
@@ -42,23 +55,9 @@ public class ContactsHelper extends HelperBase {
         click(By.linkText("home page"));
     }
 
-
     private void submitContactCreation() {
         click(By.name("submit"));
     }
 
-    private void fillContactForm(ContactData contact) {
-        type(By.name("firstname"), contact.firstname());
-        type(By.name("middlename"), contact.middlename());
-        type(By.name("lastname"), contact.lastname());
-        type(By.name("nickname"), contact.nickname());
-        type(By.name("home"), contact.home());
-    }
-
-
-    public boolean isContactPresent() {
-        openContactsPage();
-        return manager.isElementPresent(By.name("selected[]"));
-    }
 }
 
